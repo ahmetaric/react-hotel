db3ac7d355084e91f4f427fb8028517b4af2ece7
ahmetaric <ahmet.aric1965@gmail.com>
2022-08-17 09:42:02 +0300

    v-3

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
 
 const Hotel = () => {
   return (
-    <div>Hotel</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
 
-export default Hotel
+export default Hotel;
